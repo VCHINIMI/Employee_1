4a377e487813c2a540785aae85e3d12aff7eddbf
VCHINIMI <vinaychinimilli@gmail.com>
2020-09-24 06:36:59 +0530

    [VINAY]Added: EmployeeWageBuilderUC2.java

---
+++ EmployeeWageBuilderUC2.java
@@ -0,0 +1,19 @@
+public class EmployeeWageBuilderUC2{
+
+	public static void main(String args[]){
+		int FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+
+		double empCheck= Math.floor(Math.random() *10)%2;
+		if( empCheck == FULL_TIME )
+			empHrs = 8;
+			//System.out.println("present");
+		else
+			empHrs=0;
+		empWage= empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
+	}
+}
